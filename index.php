984c06f69e892685e1e5cda68017faca9e80e667
xmartina <martinsprince126@gmail.com>
2025-04-05 00:11:08 +0100

    d

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include_once 'include/config.php';
-include_once '/layout/header.php'; ?>
+include_once 'layout/header.php'; ?>
 
     <!-- Banner One Start -->
     <section class="banner-one">
