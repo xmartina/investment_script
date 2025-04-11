648eeecb8d27d75884a0f89ae50a8bb60f8aabcb
xmartina <martinsprince126@gmail.com>
2025-04-11 19:09:55 +0100

    d

--- include/config.php
+++ include/config.php
@@ -19,20 +19,13 @@ $menus_result = $conn_front->query($sql_front);
 
 $sql_front_single = "SELECT * FROM front_menus";
 $single_menu_result = $conn_front->query($sql_front_single);
-
-$about_link_name = '';
-$about_link = '';
-$register_link_name = '';
-$register_link = '';
-
-while ($row = $single_menu_result->fetch_assoc()) {
-    if ($row['menu_link'] == 'about') {
-        $about_link_name = $row['menu_name'];
-        $about_link = $row['menu_link'];
-    } elseif ($row['menu_link'] == 'register') {
-        $register_link_name = $row['menu_name'];
-        $register_link = $row['menu_link'];
-    }
+$row = $single_menu_result->fetch_assoc();
+if ($row['menu_link'] == 'about') {
+    $about_link_name = $row['menu_name'];
+    $about_link = $row['menu_link'];
+}elseif ($row['menu_link'] == 'register') {
+    $register_link_name = $row['menu_name'];
+    $register_link = $row['menu_link'];
 }
 
 
