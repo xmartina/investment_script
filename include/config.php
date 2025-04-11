6733d59dfb9df36baf62388832528c6eac1e6136
xmartina <martinsprince126@gmail.com>
2025-04-11 16:12:28 +0100

    d

--- include/config.php
+++ include/config.php
@@ -1,3 +1,15 @@
 <?php
+include_once 'db.php';
+
+$sql_front = "SELECT * FROM general_settings";
+$result = $conn_front->query($sql_front);
+$row = $result->fetch_assoc();
+$site_link = $row['site_link'];
+$site_name = $row['site_name'];
+$site_email = $row['site_email'];
+$site_phone = $row['site_phone'];
+$site_logo = $row['site_logo'];
+$site_favicon = $row['site_favicon'];
+
 const web_url = 'exodusaipro.online';
 const link = 'https://exodusaipro.online';
