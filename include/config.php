c7ea0b05ded253c3826f3451780322297bd6cc43
xmartina <martinsprince126@gmail.com>
2025-04-11 19:05:24 +0100

    d

--- include/config.php
+++ include/config.php
@@ -17,6 +17,12 @@ $site_favicon = $row['site_favicon'];
 $sql_front = "SELECT * FROM front_menus";
 $menus_result = $conn_front->query($sql_front);
 $single_menu_result = $conn_front->query($sql_front);
+
+$about_link_name = '';
+$about_link = '';
+$register_link_name = '';
+$register_link = '';
+
 while ($row = $single_menu_result->fetch_assoc()) {
     if ($row['menu_link'] == 'about') {
         $about_link_name = $row['menu_name'];
