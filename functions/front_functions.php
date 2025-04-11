9e000ede0856ae5c18655de9384278b5a4b23658
xmartina <martinsprince126@gmail.com>
2025-04-11 18:00:38 +0100

    d

--- functions/front_functions.php
+++ functions/front_functions.php
@@ -1,16 +1,19 @@
 <?php
 function list_menu($result, $active_url, $page_name) {
+    $first = true; // Flag to identify the first menu item
+
     while ($row = $result->fetch_assoc()) {
         $menu_name = $row['menu_name'];
         $menu_link = $row['menu_link'];
 
-        if (strpos($active_url, $page_name) !== false || $page_name == 'Home') {
+        if ($first && (strpos($active_url, $page_name) !== false || $page_name == 'Home')) {
             $current = 'current';
+            $first = false; // Apply only once
         } else {
             $current = '';
         }
         ?>
-        <li class="<?= $current ?> dropdown">
+        <li class="<?= $current ?>">
             <a href="<?= $menu_link ?>"><?= $menu_name ?></a>
         </li>
         <?php
