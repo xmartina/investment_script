49bb73419c05a8a4c7687a25a1f5c59eaa8c7942
xmartina <martinsprince126@gmail.com>
2025-04-17 23:43:31 +0100

    d

--- about/index.php
+++ about/index.php
@@ -1,7 +1,10 @@
 <?php
 $page_name = 'About';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/include/config.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/header.php'; ?>
+include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/header.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/breadcrumb.php';
+?>
+
 
 
 <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/layout/footer.php'; ?>
