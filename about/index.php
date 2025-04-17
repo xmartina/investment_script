6523d48101a88f98664f5beac057c832e7883110
xmartina <martinsprince126@gmail.com>
2025-04-17 23:37:32 +0100

    d

---
+++ about/index.php
@@ -0,0 +1,7 @@
+<?php
+$page_name = 'About';
+include_once 'include/config.php';
+include_once 'layout/header.php'; ?>
+
+
+<?php include_once 'layout/footer.php'; ?>
---
+++ layout/breadcrumb.php
@@ -0,0 +1,14 @@
+<!-- page-title -->
+<section class="page-title">
+    <div class="bg-layer" style="background-image: url(<?=$site_link?>/front_assets/images/background/page-title.jpg);"></div>
+    <div class="auto-container">
+        <div class="content-box">
+            <h1>About Us</h1>
+            <ul class="bread-crumb clearfix">
+                <li><a href="<?=$home['link']?>"><?=$home['name']?></a></li>
+                <li><?=$page_name?></li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!-- page-title end -->
