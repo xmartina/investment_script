fb3d1982791d7b8dadc68b6340b0b5f813d921aa
xmartina <martinsprince126@gmail.com>
2025-04-18 20:22:16 +0100

    d

--- user/dashboard.php
+++ user/dashboard.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-
-$page_name = 'Dashboard';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/include/config.php';
-include_once $_SERVER['DOCUMENT_ROOT'] . '/user/layout/header.php';
-
 if (!isset($_SESSION['user'])) {
     header('Location: /user/login');
     exit;
 }
+$page_name = 'Dashboard';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/config.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/user/layout/header.php';
+
+
 ?>
