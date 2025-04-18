a40c469b10522a9aad922b9d477f2f2900b5fcf1
xmartina <martinsprince126@gmail.com>
2025-04-18 06:51:48 +0100

    d

--- routes.php
+++ routes.php
@@ -23,15 +23,19 @@ function dispatch() {
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $uri = rtrim($uri, '/');
 
+    // 👇 Debug line: shows what URI PHP is seeing
+    echo "<pre>REQUEST_URI: " . $_SERVER['REQUEST_URI'] . "\n";
+    echo "Parsed URI: $uri\n</pre>";
+
     foreach ($routes[$method] ?? [] as $route => $callback) {
         $pattern = "@^" . preg_replace('/\{([\w]+)\}/', '([\w-]+)', $route) . "$@";
         if (preg_match($pattern, $uri, $matches)) {
-            array_shift($matches); // remove full match
+            array_shift($matches);
             return call_user_func_array($callback, $matches);
         }
     }
 
-    // 404 handler
     http_response_code(404);
     echo "404 Not Found";
 }
+
