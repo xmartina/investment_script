09ba162e6371601be18a47406632c99dd256ccd5
xmartina <martinsprince126@gmail.com>
2025-04-27 09:08:36 +0100

    d

--- user/dashboard.php
+++ user/dashboard.php
@@ -190,7 +190,6 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/user/layout/header.php';
                     <!-- TradingView Widget BEGIN -->
                     <div class="tradingview-widget-container">
                         <div class="tradingview-widget-container__widget"></div>
-                        <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
                         <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js" async>
                             {
                                 "symbols": [
