8328aec63c3ccdf7c35e81c7a3e9d9cb8fd31b3e
xmartina <martinsprince126@gmail.com>
2025-05-01 10:41:59 +0100

    d

--- functions/models/transactions.php
+++ functions/models/transactions.php
@@ -31,38 +31,6 @@ function selectAllTransactions($conn_back) {
 //    return $transactions;
 //}
 
-$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
-$user_id = isset($_GET['user_id']) ? (int)$_GET['user_id'] : 0;
-$per_page = 10;
-
-$offset = ($page - 1) * $per_page;
-$transactions = selectTransactionsByUserIdPaginated($conn_back, $user_id, $offset, $per_page);
-$totalTransactions = countTransactionsByUserId($conn_back, $user_id);
-
-// Generate HTML for new rows
-ob_start();
-foreach ($transactions as $transaction) {
-    // Determine currency, type, status as before
-    ?>
-    <tr>
-        <td><?= $transaction['reference_id'] ?></td>
-        <td><?= $trans_currency . $transaction['amount'] ?></td>
-        <td><?= $trans_type ?></td>
-        <td><?= $trans_status ?></td>
-        <td><?= $transaction['date_time'] ?></td>
-    </tr>
-    <?php
-}
-$rows = ob_get_clean();
-
-$response = [
-    'rows' => $rows,
-    'hasMore' => ($page * $per_page) < $totalTransactions,
-    'nextPage' => $page + 1
-];
-
-header('Content-Type: application/json');
-echo json_encode($response);
 // Function to update a transaction
 function updateTransaction($conn_back,$transaction_id, $new_status) {
     $sql = "UPDATE transactions SET status = '$new_status' WHERE transaction_id = '$transaction_id'";
