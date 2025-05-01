4bb7a31481b2c4a74465bb9596e65ee6bb7babb4
xmartina <martinsprince126@gmail.com>
2025-05-01 17:02:36 +0100

    d

--- user/deposit.php
+++ user/deposit.php
@@ -34,7 +34,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/user/layout/breadcumb.php';
                 <div class="tab-content">
                     <div id="step-1" class="tab-pane px-0" role="tabpanel" aria-labelledby="step-1">
                         <div class="row my-2">
-                            <div class="col-6 col-md-6 col-lg-6 col-xl-6 mb-4">
+                            <div class="col-12 col-md-6 col-lg-6 col-xl-6 mb-4">
                                 <div class="card text-center bg-theme-1-subtle theme-green h-100 selectable">
                                     <div class="card-body">
                                         <div class="mb-3">
