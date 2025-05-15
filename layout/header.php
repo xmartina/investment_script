3e38548805e6c6a6f1d908c555bf1e601e569789
xmartina <martinsprince126@gmail.com>
2025-05-15 21:51:23 +0100

    d

--- layout/header.php
+++ layout/header.php
@@ -37,7 +37,7 @@
             max-width: 120px ;
         }
         .pricing-block-one {
-            box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px !important;
+            box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px !important;
         }
     </style>
 
