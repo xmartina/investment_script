88e5a0a9b7b9236148578ae7c1ce150a93db2130
xmartina <martinsprince126@gmail.com>
2025-05-15 21:52:06 +0100

    d

--- layout/header.php
+++ layout/header.php
@@ -38,6 +38,7 @@
         }
         .pricing-block-one {
             box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px !important;
+            border-radius: 10px;
         }
     </style>
 
